a120170df61a4a57c37888577085e23b104ed1b3
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2022-12-20 00:21:36 +0530

    blob-test

--- src/components/Space.jsx
+++ src/components/Space.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import '../stylesheets/space.css'
-import { OrbitControls, Stars } from '@react-three/drei'
+import { OrbitControls, Stars, Sphere, MeshDistortMaterial } from '@react-three/drei'
+
+function Portal() {
+  return(
+    <Sphere visible args={[1.5, 250, 350]}>
+      <MeshDistortMaterial color={0x8352fd} attach='material' 
+        distort={0.3} speed={1.7} roughness={0}
+      />
+    </Sphere>
+  )
+}
 
 function Space() {
   return (
     <Canvas className='spaceCanvas'>
       <OrbitControls enableZoom={false}/>
+      <ambientLight intensity={0.6} />
+      <spotLight position={[10, 20, 10]} angle={3} />
+      <Portal />
       <Stars />
     </Canvas>
   )
