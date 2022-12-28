03ce24fdac59fcd7b0fbd66d4c0254120264b3d8
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2022-12-28 23:36:47 +0530

    planets-added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,4 @@
-import { Sphere } from '@react-three/drei'
 import React from 'react'
-import Intro from './Intro'
 import Space from './Space'
 
 
@@ -8,7 +6,6 @@ function Home() {
   return (
     <div>
         <Space />
-        <Intro />
     </div>
   )
 }
--- src/components/Space.jsx
+++ src/components/Space.jsx
@@ -1,8 +1,9 @@
 import React, {useRef, Suspense} from 'react'
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
-import { Box, OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import { Box, FirstPersonControls, OrbitControls, PerspectiveCamera, TransformControls } from '@react-three/drei'
 import * as THREE from 'three'
 import { TextureLoader } from 'three'
+import { useNavigate } from 'react-router-dom'
 
 
 const extraStyle={
@@ -13,7 +14,20 @@ const extraStyle={
     top: 0,
 }
 
+const Planet = ({pos, txt, arg, click}) => {
+  const texture =  useLoader(TextureLoader, txt);
+
+  return(
+    <mesh position={pos} onClick={click} >
+      <sphereBufferGeometry args={arg} attach='geometry' />
+      <meshBasicMaterial attach='material' map={texture} />
+    </mesh>
+  )
+}
+
 function Space() {
+
+  const navigate = useNavigate();
 
   const dn = useLoader(TextureLoader, './bottom.png');
   const up = useLoader(TextureLoader, './top.png');
@@ -24,8 +38,12 @@ function Space() {
   
   return (
     <Canvas className='skyBox' style={extraStyle}>
-        <OrbitControls enableZoom={false}/>
+        {/* <OrbitControls enableZoom={false}/> */}
+        <FirstPersonControls lookSpeed={0.05} />
         <Suspense fallback='loading'>
+          <Planet pos={[25, 0, -15]} arg={[3.2]} txt='./mercury.jpg' click={() => navigate('/World')}/>
+          <Planet pos={[0, 0, -15]} arg={[2.2]} txt='./jupiter.jpg' click={() => navigate('/World')}/>
+          <Planet pos={[-15, 0, -15]} arg={[1.2]} txt= './saturn.jpg' click={() => navigate('/World')}/>
           <Box args={[1000, 1000, 1000]}>
             <meshBasicMaterial side={THREE.BackSide} attach="material-0" map={rt} />
             <meshBasicMaterial side={THREE.BackSide} attach="material-1" map={lt} />
